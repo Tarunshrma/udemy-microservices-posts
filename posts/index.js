709554ce3ad6f58ddb3d85b2cbddbd0c546ed829
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2021-08-16 10:48:19 +0530

    Added kubenetes config for react client

--- posts/index.js
+++ posts/index.js
@@ -9,11 +9,12 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors());
 
+//TODO: CAN BE REMOVED, NOT BEING USE
 app.get('/posts', (req, res) => {
   res.send(posts)
 })
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex')
   const { title } = req.body
 
